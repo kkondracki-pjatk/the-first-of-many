9dd15d99c134338bf36aacffc8d1d6d50e5b8efa
Kacper Kondracki <s33857@pjwstk.edu.pl>
2025-03-06 11:10:37 +0100

    edycja glownego kodu

--- makapaka/src/Main.java
+++ makapaka/src/Main.java
@@ -2,9 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-        System.out.println("i = " + i);
-        }
+        System.out.printf("Hello!");
+        System.out.printf("and");
+        System.out.printf("welcome!");
+        System.out.printf("larry.");
     }
-}
